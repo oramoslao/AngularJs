3fe2d0ce4e0328b0f542bc36fb19ca9d88d4d73d
Octavio Ramos <oramoslao@gmail.com>
2020-04-25 16:37:34 -0500

    directiva personalizada 1

--- app/app.js
+++ app/app.js
@@ -5,10 +5,19 @@ myApp.controller("HomeCtr", HomeController);
 HomeController.$inject = ["$scope"];
 
 function HomeController($scope) {
-  $scope.users = [
-    { name: "Octavio", enabled: true },
-    { name: "Raul", enabled: false },
-    { name: "Maria", enabled: true },
-    { name: "Luisa", enabled: false },
-  ];
+  $scope.user = {
+    name: "Octavio",
+    lastname: "Ramos La O",
+  };
 }
+//A: Atriburo
+//E: Elemento
+myApp.directive("fullName", function () {
+  return {
+    restrict: "AE",
+    template: "Nombre completo: {{ user.name }} {{ user.lastname }}",
+    scope: {
+      user: "=info",
+    },
+  };
+});
