94d4e878b006bb71889c7abaeeacb6a13d8d8dfd
Octavio Ramos <oramoslao@gmail.com>
2020-04-25 17:33:25 -0500

    directiva myCurrentTime

--- app/app.js
+++ app/app.js
@@ -9,15 +9,49 @@ function HomeController($scope) {
     name: "Octavio",
     lastname: "Ramos La O",
   };
+  $scope.format = "M/d/yy h:mm:ss a";
 }
 //A: Atriburo
 //E: Elemento
 myApp.directive("fullName", function () {
   return {
     restrict: "AE",
-    template: "Nombre completo: {{ user.name }} {{ user.lastname }}",
+    templateUrl: "views/full-name.html",
     scope: {
       user: "=info",
     },
   };
 });
+
+myApp.directive("myCurrentTime", [
+  "$interval",
+  "dateFilter",
+  function ($interval, dateFilter) {
+    return {
+      link: linkFnc,
+    };
+
+    function linkFnc(scope, element, attrs) {
+      var format, timeoutId;
+
+      function updateTime() {
+        element.text(dateFilter(new Date(), format));
+      }
+
+      scope.$watch(attrs.myCurrentTime, function (newValue, oldValue) {
+        format = newValue;
+        updateTime();
+      });
+
+      element.on("$destroy", function () {
+        $interval.cancel(timeoutId);
+      });
+
+      // iniciar el proceso de actualización de la interfaz de usuario;
+      // guardar el tiempo de espera Id para cancelar
+      timeoutId = $interval(function () {
+        updateTime();
+      }, 1000);
+    }
+  },
+]);
